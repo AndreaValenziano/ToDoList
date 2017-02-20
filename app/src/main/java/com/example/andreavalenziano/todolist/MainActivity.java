cf4ddda8c566074df95a0dd9a27da8a90f688384
AndreaValenziano <valenzianoa@gmail.com>
2017-02-20 09:56:38 +0100

    Main Activity

---
+++ app/src/main/java/com/example/andreavalenziano/todolist/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.andreavalenziano.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AndreaValenziano on 20/02/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
